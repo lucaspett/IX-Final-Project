9ab8ad2fb3ff63db03072525ea330fdba2172ea9
lucaspett <lucaspettinato2018@u.northwestern.edu>
2015-08-02 17:06:54 +0200

    Finished basic code of Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,4 @@
 class Job < ActiveRecord::Base
+	validates :title, :description, :qualifications, :contact, :date_published, presence: true
+	validates :pay, numericality: {greater_than_or_equal_to: 1}
 end
